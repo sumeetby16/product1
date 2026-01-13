f69243d42810c6d38b5318392bcdcd471c363fff
sumeetby16 <sumeetby16@gmail.com>
2026-01-13 20:01:14 +0530

    Create product.py

---
+++ product.py
@@ -0,0 +1,7 @@
+def get_product_details(product_id, name, quantity, price):
+    return (
+        f"Product ID: {product_id}\n"
+        f"Product Name: {name}\n"
+        f"Quantity: {quantity}\n"
+        f"Price: {price}"
+    )
